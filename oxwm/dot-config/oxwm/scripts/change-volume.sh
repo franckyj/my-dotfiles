bdc20008bb57a4c075fa30a9f1d27703518699a4
zibbble glitch <zibbbleglitch@gmail.com>
2026-07-28 16:12:51 -0400

    tweak volume script

--- oxwm/dot-config/oxwm/scripts/change-volume.sh
+++ oxwm/dot-config/oxwm/scripts/change-volume.sh
@@ -2,7 +2,19 @@
 
 function send_notification() {
   volume=$(pamixer --get-volume)
-  dunstify -a "changevolume" -u low -r "9993" -h int:value:"$volume" -i "volume-$1" "Volume: ${volume}%" -t 2000
+
+  # if the volume is between 0 and 33, use the low volume icon
+  if [ "$volume" -le 33 ]; then
+    icon="audio-volume-low"
+  # if the volume is between 34 and 66, use the medium volume icon
+  elif [ "$volume" -le 66 ]; then
+    icon="audio-volume-medium"
+  # if the volume is between 67 and 100, use the high volume icon
+  else
+    icon="audio-volume-high"
+  fi
+
+  dunstify -a "changevolume" -u low -r "9993" -h int:value:"$volume" -i $icon "Volume: ${volume}%" -t 2000
 }
 
 case $1 in
@@ -10,19 +22,19 @@ up)
   # Set the volume on (if it was muted)
   pamixer -u
   pamixer -i 2 --allow-boost
-  send_notification $1
+  send_notification
   ;;
 down)
   pamixer -u
   pamixer -d 2 --allow-boost
-  send_notification $1
+  send_notification
   ;;
 mute)
   pamixer -t
   if $(pamixer --get-mute); then
-    dunstify -i volume-mute -a "changevolume" -t 2000 -r 9993 -u low "Muted"
+    dunstify -i audio-volume-muted -a "changevolume" -t 2000 -r 9993 -u low "Muted"
   else
-    send_notification up
+    send_notification
   fi
   ;;
 esac
