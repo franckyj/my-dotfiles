79b257b6b8d07f9f42f7f1e160f508034406229f
zibbble glitch <zibbbleglitch@gmail.com>
2026-07-28 16:50:10 -0400

    add new power menu

--- oxwm/dot-config/oxwm/scripts/power.sh
+++ oxwm/dot-config/oxwm/scripts/power.sh
@@ -1,24 +1,100 @@
 #!/usr/bin/env bash
 
-# Simple script to handle a DIY shutdown menu with rofi.
+## Author : Aditya Shakya (adi1090x)
+## Github : @adi1090x
 #
-# Requirements:
-# - rofi
-# - systemd (can be replaced for other init systems if needed)
+## Rofi   : Power Menu
 #
-# Instructions:
-# - Save this file as power.sh
-# - Make it executable: chmod +x /path/to/power.sh
-# - Run it
+## Available Styles
 
-ROFI_THEME="$HOME/.config/rofi/power.rasi"
+# Current Theme
+dir="$HOME/.config/rofi"
+theme='power'
 
-chosen=$(echo -e "[Cancel]\nLogout\nShutdown\nReboot" | \
-    rofi -dmenu -i -p "Power Menu" -line-padding 4 -hide-scrollbar -theme "$ROFI_THEME")
+# CMDs
+uptime="`uptime -p | sed -e 's/up //g'`"
+host=`hostname`
 
-case "$chosen" in
-    "Logout") pkill oxwm ;;
-    "Shutdown") systemctl poweroff ;;
-    "Reboot") systemctl reboot ;;
-    *) exit 0 ;; # Exit on cancel or invalid input
+# Options
+shutdown=' Shutdown'
+reboot=' Reboot'
+# lock=' Lock'
+suspend=' Suspend'
+logout=' Logout'
+yes=' Yes'
+no=' No'
+
+# Rofi CMD
+rofi_cmd() {
+  rofi -dmenu \
+    -p "$host" \
+    -mesg "Uptime: $uptime" \
+    -theme ${dir}/${theme}.rasi
+}
+
+# Confirmation CMD
+confirm_cmd() {
+  rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
+    -theme-str 'mainbox {children: [ "message", "listview" ];}' \
+    -theme-str 'listview {columns: 2; lines: 1;}' \
+    -theme-str 'element-text {horizontal-align: 0.5;}' \
+    -theme-str 'textbox {horizontal-align: 0.5;}' \
+    -dmenu \
+    -p 'Confirmation' \
+    -mesg 'Are you Sure?' \
+    -theme ${dir}/${theme}.rasi
+}
+
+# Ask for confirmation
+confirm_exit() {
+  echo -e "$yes\n$no" | confirm_cmd
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+  # $lock\n
+  echo -e "$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+  selected="$(confirm_exit)"
+  if [[ "$selected" == "$yes" ]]; then
+    if [[ $1 == '--shutdown' ]]; then
+      systemctl poweroff
+    elif [[ $1 == '--reboot' ]]; then
+      systemctl reboot
+    elif [[ $1 == '--suspend' ]]; then
+      pamixer -m
+      systemctl suspend
+    elif [[ $1 == '--logout' ]]; then
+      pkill oxwm
+    fi
+  else
+    exit 0
+  fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+    run_cmd --shutdown
+        ;;
+    $reboot)
+    run_cmd --reboot
+        ;;
+    # $lock)
+    # if [[ -x '/usr/bin/betterlockscreen' ]]; then
+    #   betterlockscreen -l
+    # elif [[ -x '/usr/bin/i3lock' ]]; then
+    #   i3lock
+    # fi
+    #     ;;
+    $suspend)
+    run_cmd --suspend
+        ;;
+    $logout)
+    run_cmd --logout
+        ;;
 esac
