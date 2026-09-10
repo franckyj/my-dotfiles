7f26199a29aa07879d9fbe92e1604958f8adaa4a
zibbble glitch <zibbbleglitch@gmail.com>
2026-09-09 20:07:50 -0400

    update config to take into account the noctalia generated colors

--- hyprland/dot-config/hypr/config/looknfeel.lua
+++ hyprland/dot-config/hypr/config/looknfeel.lua
@@ -1,19 +1,10 @@
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 
-local active_border_color = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 }
-local inactive_border_color = "rgba(595959aa)"
-
 hl.config({
   general = {
     gaps_in = 5,
     gaps_out = 10,
     border_size = 2,
-
-    col = {
-      active_border = active_border_color,
-      inactive_border = inactive_border_color,
-    },
-
     resize_on_border = false,
     allow_tearing = false,
     layout = "dwindle",
@@ -32,11 +23,6 @@ hl.config({
   },
 
   group = {
-    col = {
-      border_active = active_border_color,
-      border_inactive = inactive_border_color,
-    },
-
     groupbar = {
       font_size = 12,
       font_family = "monospace",
@@ -47,12 +33,6 @@ hl.config({
       height = 22,
       gaps_in = 5,
       gaps_out = 0,
-      text_color = "rgb(ffffff)",
-      text_color_inactive = "rgba(ffffff90)",
-      col = {
-        active = "rgba(00000040)",
-        inactive = "rgba(00000020)",
-      },
       gradients = true,
       gradient_rounding = 0,
       gradient_round_only_edges = false,
